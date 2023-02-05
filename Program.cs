b4f356bf0443b14beaa1811546cfead2ee50fc8c
Carlos García Murillo <c.g.murillo91@gmail.com>
2023-02-05 18:37:41 +0100

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿int num1, num2;
+
+Console.WriteLine("INTRODUCE PRIMER NUMERO:");
+num1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("INTRODUCE SEGUNDO NUMERO:");
+num2 = int.Parse(Console.ReadLine());
+
+int suma = num1 + num2;
+int resta = num1 - num2;
+int producto = num1 * num2;
+int cociente = num1 / num2;
+int residuo = num1 % num2;
+
+Console.WriteLine("PRIMER NUMERO: " + num1);
+Console.WriteLine("SEGUNDO NUMERO: " + num2);
+Console.WriteLine("LA SUMA ES: " + suma);
+Console.WriteLine("LA RESTA ES: " + num1 + " - " + num2 + " = " + resta);
+Console.WriteLine("LA MULTIPLICACION ES: " + producto);
+Console.WriteLine("LA DIVISION ES: " + cociente);
+Console.WriteLine("EL RESIDUO ES: " + residuo);
+
+Console.WriteLine("Pulse una tecla:");
+Console.ReadLine();
